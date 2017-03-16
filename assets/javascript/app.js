bc57bc4ce926aed9d38a43efaac8d53d52de1f41
Jimel J. Joseph <jaydev@Jimels-MacBook-Pro-2.local>
2017-03-16 12:48:05 -0400

    finalizing touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,16 +9,16 @@ var unAnswered = 0;
 $(".mainBody").hide();
 $(".answers").hide();
 
-$("#buttonStart").on("click", function(){
+$("#buttonStart").on("click", function() {
     $(".splash").hide();
     $(".mainBody").show();
     timer = 150;
     intervalId = setInterval(myTimer, 1000);
-} )
+})
 
-$("#buttonDone").on("click", function(){
-  done();
-  clearInterval(intervalId);
+$("#buttonDone").on("click", function() {
+    done();
+    clearInterval(intervalId);
 
 })
 
@@ -30,53 +30,49 @@ function myTimer() {
 
     timer--;
     console.log(timer);
-    $("#timer").html(timer +" Seconds");
+    $("#timer").html(timer + " Seconds");
 
     if (timer === 0) {
-      done();
-      clearInterval(intervalId);
-      console.log("I AM HERE");
+        done();
+        clearInterval(intervalId);
+        console.log("I AM HERE");
     }
 
 }
 
 function validation(Boolean) {
-  if (Boolean)
-  {
-    correctAnswers++;
-    console.log("correct",correctAnswers);
-    console.log("INSIDE validation");
+    if (Boolean) {
+        correctAnswers++;
+        console.log("correct", correctAnswers);
+        console.log("INSIDE validation");
 
-  }else if ($('[type="radio"]').is(":not(':checked')")) {
-      unAnswered++
-      console.log("unAnswered", unAnswered);
-      console.log("inside not", unAnswered);
-  }
-
-
-  else{
-    wrongAnswers++
-    console.log("wrong",wrongAnswers);
-  }
+    } else if ($('[type="radio"]').is(":not(':checked')")) {
+        unAnswered++
+        console.log("unAnswered", unAnswered);
+        console.log("inside not", unAnswered);
+    } else {
+        wrongAnswers++
+        console.log("wrong", wrongAnswers);
+    }
 
 
 }
 
 function done() {
-  console.log("here here");
-  $(".mainBody").hide();
-  $(".answers").show();
-  validation($('[value="10000"]').is(':checked'));
-  validation($('[value="1966"]').is(':checked'));
-  validation($('[value="elanor rigby"]').is(':checked'));
-  validation($('[value="the end"]').is(':checked'));
-  validation($('[value="king lear"]').is(':checked'));
-  validation($('[value="allan williams"]').is(':checked'));
-  validation($('[value="paul mccartney"]').is(':checked'));
+    console.log("here here");
+    $(".mainBody").hide();
+    $(".answers").show();
+    validation($('[value="10000"]').is(':checked'));
+    validation($('[value="1966"]').is(':checked'));
+    validation($('[value="elanor rigby"]').is(':checked'));
+    validation($('[value="the end"]').is(':checked'));
+    validation($('[value="king lear"]').is(':checked'));
+    validation($('[value="allan williams"]').is(':checked'));
+    validation($('[value="paul mccartney"]').is(':checked'));
 
-$(".correct").html("Correct Answers: "+correctAnswers);
-$(".wrong").html("Wrong Answers: "+wrongAnswers);
-$(".un").html("Unanswered: "+unAnswered);
+    $(".correct").html("Correct Answers: " + correctAnswers);
+    $(".wrong").html("Wrong Answers: " + wrongAnswers);
+    $(".un").html("Unanswered: " + unAnswered);
 
 
 }
